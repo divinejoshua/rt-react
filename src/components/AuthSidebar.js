e19ac19c568d2a4bfcb6c39d99f6143d9b229b66
divinejoshua <divineuche399@gmail.com>
2022-07-20 19:21:27 +0100

    Made changes

--- src/components/AuthSidebar.js
+++ src/components/AuthSidebar.js
@@ -19,11 +19,14 @@ export default function AuthSidebar() {
             <div>
                 <div className='auth-info-box border border-gray-100 shadow-md pt-5 pl-5 border-radius-10 pb-5 flex'>
                     <img src={authpaper} className="ml-2" width={50} height={50} alt="Runtransfer"/>
-                    <div>
-                        <p className='font-size-x-small'>Transactions verified</p>
+                    <div className='mt-1 ml-5'>
+                        <p className='font-size-x-small font-color-777'>Transactions verified</p>
                         <p className='font-size-medium'>72</p>
                     </div>
                 </div>
+                <p className='font-color-777 font-size-medium'>
+                    You’re a few clicks away from accepting and verifying transfers.
+                </p>
 
             </div>
            
