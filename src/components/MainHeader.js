d17fdd1855cdf9ef0b5ad55bb8a45a6079b81a56
divinejoshua <divineuche399@gmail.com>
2022-07-22 21:12:01 +0100

    Made changes

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -4,7 +4,7 @@ export default function MainHeader() {
   return (
     // Nav bar 
     <nav className='mainheader border-b flex'>
-      <div>
+      <div className='header-content border'>
         <h1 className='text-2xl font-bold'>Wetroverse</h1>
       </div>
     </nav>
