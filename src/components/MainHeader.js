ee7ffc7d7b27d2796a9cc543acfaa012ce3e5bf2
divinejoshua <divineuche399@gmail.com>
2022-07-22 21:28:44 +0100

    Added search box

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -7,7 +7,12 @@ export default function MainHeader() {
 
       <div className='header-content w-full flex flex-row'>
         <h1 className='text-3xl font-bold mt-4 basis-1/3'>Wetroverse</h1>
-        <h1 className='text-3xl font-bold mt-4 basis-1/3'>Wetroverse</h1>
+
+        {/* Search box  */}
+        <div className=' basis-1/3 border pt-2'>
+          <center><input className='search-box' type='text' placeholder='Search'></input></center>
+        </div>
+
         <h1 className='text-3xl font-bold mt-4 basis-1/3'>Wetroverse</h1>
       </div>
 
