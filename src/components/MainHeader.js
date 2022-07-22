3702136ceaef8cf4e06ad92d8766f345b8fc89e6
divinejoshua <divineuche399@gmail.com>
2022-07-22 22:23:30 +0100

    Started main body

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function MainHeader() {
   return (
+
+ 
     // Nav bar 
     <nav className='mainheader border-b w-full'>
 
@@ -18,8 +21,13 @@ export default function MainHeader() {
         </div>
 
           <div className='basis-1/3'>
-            <button className='header-btn-full float-right mt-4 pl-5 pr-5 rounded-lg bg-default text-white'>Register</button>
-            <button className='header-btn-float float-right mt-4 pl-5 pr-5 rounded-lg border mr-6'>Login</button>
+            <Link to="/accounts/register">
+              <button className='header-btn-full float-right mt-4 pl-5 pr-5 rounded-lg bg-default text-white'>Register</button>
+            </Link>
+
+            <Link to="/accounts/login">
+              <button className='header-btn-float float-right mt-4 pl-5 pr-5 rounded-lg border mr-6'>Login</button>
+            </Link>
           </div>
       </div>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import MainHeader from "../components/MainHeader";
 
 export default function HomeView() {
 
+// STATES 
+const [isLoadingPage, setisLoadingPage] = useState(true);
+
+  
+//USE EFFECT
+useEffect(() => {
+  console.log("home")
+
+  //Set loding page to false
+  setTimeout(() => {  setisLoadingPage(false) }, 3000);
+
+
+  return () => {
+  }
+}, [])
+
 
   return (
     <div>
+
+      {/* Top loader  */}
+      {isLoadingPage && 
+        <div className="linear-activity">
+          <div className="indeterminate"></div>
+        </div>
+      }
+
+      {/* Main header  */}
       <MainHeader/>
+
+      <div className="container mainbody grid grid-cols-3 gap-4">
+
+        <div class="...">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        <div class="...">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        <div class="...">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+
+      </div>
      
     </div>
     
