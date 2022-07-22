0cf81ac4df4019095bee25f810a32546d16c0029
divinejoshua <divineuche399@gmail.com>
2022-07-22 21:45:04 +0100

    Adding header buttons

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -6,14 +6,20 @@ export default function MainHeader() {
     <nav className='mainheader border-b w-full'>
 
       <div className='header-content w-full flex flex-row'>
-        <h1 className='text-3xl font-bold mt-4 basis-1/3'>Wetroverse</h1>
+        <h1 className='text-3xl  mt-4 basis-1/3'>Wetro<span className='text-3xl text-default font-bold'>/verse</span></h1>
 
         {/* Search box  */}
         <div className=' basis-1/3 pt-3'>
-          <center><input className='search-box pl-5 rounded-lg' type='text' placeholder='Search'></input></center>
+          <center>
+            <input className='search-box pl-5 rounded-lg focus:outline-none focus:border-default focus:ring-default focus:ring-0.5 focus:border-100 transition duration-0 hover:duration-150' 
+                    type='text' placeholder='Search'>
+            </input>
+            </center>
         </div>
 
-        <h1 className='text-3xl font-bold mt-4 basis-1/3'>Wetroverse</h1>
+          <div className='basis-1/3'>
+            <button className='header-login-btn float-right mt-4 pl-5 pr-5'>Login</button>
+          </div>
       </div>
 
     </nav>
