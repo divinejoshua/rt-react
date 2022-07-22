68dab7e822a9fa67271c1f388cc6d52fef2345b5
divinejoshua <divineuche399@gmail.com>
2022-07-22 21:15:56 +0100

    Made changes

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -3,10 +3,12 @@ import React from 'react'
 export default function MainHeader() {
   return (
     // Nav bar 
-    <nav className='mainheader border-b flex'>
-      <div className='header-content border'>
-        <h1 className='text-2xl font-bold'>Wetroverse</h1>
+    <nav className='mainheader border-b w-full'>
+
+      <div className='header-content w-full'>
+        <h1 className='text-3xl font-bold mt-4'>Wetroverse</h1>
       </div>
+
     </nav>
   )
 }
