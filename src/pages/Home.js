6145ca8240d1c49d9a82697e19be9f4261b5a9a4
divinejoshua <divineuche399@gmail.com>
2022-07-23 00:59:49 +0100

    Added images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -93,31 +93,86 @@ useEffect(() => {
 
             {/* Profile card */}
             <div class="ml-3">
-              <div className="flex">
+              <div className="flex mt-3">
                 <img src="https://robohash.org/namquaerataut.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full profile-card-image"></img>
                 <div>
                   <h4 className="font-bold mt-11 ml-2">divine.er</h4>
-                  <h4 className="ml-2 font-color-777">Divine Erhomonsele</h4>
+                  <h4 className="ml-2 font-color-777 font-size-x-small">Divine Erhomonsele</h4>
                 </div>
-              <button className='mt-12 ml-12 add-post-btn bg-blue-400 text-white float-right mt-4 pl-5 pr-5 rounded-lg border mr-6'>Add post</button>
+              <button className='mt-12 ml-20 add-post-btn float-right mt-4 pl-5 pr-5 rounded-lg border'>Add post</button>
 
               </div>
             </div>
 
 
           {/* Posts  */}
-          <div class="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+          <div class="mt-4">
+            <div className="post-card border rounded-lg">
+              <img src="https://picsum.photos/200/300?" className="post-img" alt="wetroverse"></img>
+            </div>
+          </div>
           <div class="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
 
           {/* Users list  */}
           <div class="mt-4 ml-3">
+            <p className="font-color-777 mb-3">Suggestions for you</p>
               <div className="flex">
-                    <img src="https://robohash.org/namquaerataut.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full users-card-image"></img>
+                    <img src="https://robohash.org/quisequienim.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full users-card-image"></img>
                     <div>
-                      <h4 className="font-bold mt-11 ml-2">divine.er</h4>
-                      <h4 className="ml-2 font-color-777">Divine Erhomonsele</h4>
+                      <h4 className="font-bold mt-6 ml-2">divine.er</h4>
+                      <h4 className="ml-2 font-color-777 font-size-x-small">Divine Erhomonsele</h4>
                     </div>
-                  <button className='mt-12 ml-12 add-post-btn bg-blue-400 text-white float-right mt-4 pl-5 pr-5 rounded-lg border mr-6'>Add post</button>
+                  <p className='ml-36 text-sky-400 font-bold float-right mt-8 mr-6 cursor-pointer'>Follow</p>
+
+              </div>
+
+              <div className="flex">
+                    <img src="https://robohash.org/quisequienim.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full users-card-image"></img>
+                    <div>
+                      <h4 className="font-bold mt-6 ml-2">divine.er</h4>
+                      <h4 className="ml-2 font-color-777 font-size-x-small">Divine Erhomonsele</h4>
+                    </div>
+                  <p className='ml-36 text-sky-400 font-bold float-right mt-8 mr-6 cursor-pointer'>Follow</p>
+
+              </div>
+
+              <div className="flex">
+                    <img src="https://robohash.org/quisequienim.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full users-card-image"></img>
+                    <div>
+                      <h4 className="font-bold mt-6 ml-2">divine.er</h4>
+                      <h4 className="ml-2 font-color-777 font-size-x-small">Divine Erhomonsele</h4>
+                    </div>
+                  <p className='ml-36 text-sky-400 font-bold float-right mt-8 mr-6 cursor-pointer'>Follow</p>
+
+              </div>
+
+              <div className="flex">
+                    <img src="https://robohash.org/quisequienim.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full users-card-image"></img>
+                    <div>
+                      <h4 className="font-bold mt-6 ml-2">divine.er</h4>
+                      <h4 className="ml-2 font-color-777 font-size-x-small">Divine Erhomonsele</h4>
+                    </div>
+                  <p className='ml-36 text-sky-400 font-bold float-right mt-8 mr-6 cursor-pointer'>Follow</p>
+
+              </div>
+
+              <div className="flex">
+                    <img src="https://robohash.org/quisequienim.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full users-card-image"></img>
+                    <div>
+                      <h4 className="font-bold mt-6 ml-2">divine.er</h4>
+                      <h4 className="ml-2 font-color-777 font-size-x-small">Divine Erhomonsele</h4>
+                    </div>
+                  <p className='ml-36 text-sky-400 font-bold float-right mt-8 mr-6 cursor-pointer'>Follow</p>
+
+              </div>
+
+              <div className="flex">
+                    <img src="https://robohash.org/quisequienim.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full users-card-image"></img>
+                    <div>
+                      <h4 className="font-bold mt-6 ml-2">divine.er</h4>
+                      <h4 className="ml-2 font-color-777 font-size-x-small">Divine Erhomonsele</h4>
+                    </div>
+                  <p className='ml-36 text-sky-400 font-bold float-right mt-8 mr-6 cursor-pointer'>Follow</p>
 
               </div>
           </div>
