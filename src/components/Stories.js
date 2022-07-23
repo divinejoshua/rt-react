7873bfcd20658b47f7002160784a7159f4e8265f
divinejoshua <divineuche399@gmail.com>
2022-07-23 17:22:26 +0100

    Made changes

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -5,7 +5,7 @@ export default function Stories() {
     <div className="stories-card border rounded-lg col-span-2 flex pl-4">
         <div className="mr-4 ">
         <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
-        <p className="text-center mt-2">Divine</p>
+        <p className="text-center mt-2">Divine`0</p>
         </div>
         <div className="mr-4 ">
         <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
