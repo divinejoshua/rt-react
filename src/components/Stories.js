57077cd8b41a92a094cf2a1be6cdeb1e132aec6c
divinejoshua <divineuche399@gmail.com>
2022-07-23 17:45:03 +0100

    Made changes to stories

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -23,13 +23,13 @@ export default function Stories() {
 
   return (
     <div className="stories-card border rounded-lg col-span-2 flex pl-4">
-        {Array.isArray(users.users) ? users.users.map(user =>{
+        {Array.isArray(users.users) ? users.users.map(user => (
             <div className="mr-4" key={user.id}>
                 <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
                 <p className="text-center mt-2">{user.username}</p>
             </div>
 
-        }) : null}
+        )) : null }
         
                   
     </div>
