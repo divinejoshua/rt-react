323cf93185bcfec9e8c439652be79712807ae115
divinejoshua <divineuche399@gmail.com>
2022-07-23 17:27:45 +0100

    made changes

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,40 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import useUsersList from '../utils/useUsersList'
+
 
 export default function Stories() {
+
+  // STATES 
+  const {getUsers, data : users, isPending, messageSuccess, messageError} = useUsersList()
+  // METHODS 
+
+
+  //USE EFFECT
+  useEffect(() => {
+
+    // Get users list 
+    getUsers()
+
+    return () => {
+    };
+  }, []);
+
+
+
   return (
     <div className="stories-card border rounded-lg col-span-2 flex pl-4">
-        <div className="mr-4 ">
-        <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
-        <p className="text-center mt-2">Divine`0</p>
-        </div>
-        <div className="mr-4 ">
-        <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
-        <p className="text-center mt-2">Divine</p>
-        </div>
-        <div className="mr-4 ">
-        <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
-        <p className="text-center mt-2">Divine</p>
-        </div>
-        <div className="mr-4 ">
-        <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
-        <p className="text-center mt-2">Divine</p>
-        </div>
-        <div className="mr-4 ">
-        <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
-        <p className="text-center mt-2">Divine</p>
-        </div>
-        <div className="mr-4 ">
-        <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
-        <p className="text-center mt-2">Divine</p>
-        </div>
-        <div className="mr-4 ">
-        <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
-        <p className="text-center mt-2">Divine</p>
-        </div>
-        <div className="mr-4 ">
-        <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
-        <p className="text-center mt-2">Divine</p>
-        </div>
+        {Array.isArray(users.users) ? users.users.map(user =>{
+            <div className="mr-4" key={user.id}>
+                <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
+                <p className="text-center mt-2">{user.username}</p>
+            </div>
+
+        }) : null}
+        
                   
     </div>
   )
