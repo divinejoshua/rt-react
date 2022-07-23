042941e82edf7461fe2e4ac1fcd4576332830e8c
divinejoshua <divineuche399@gmail.com>
2022-07-23 13:16:11 +0100

    made changes to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -58,15 +58,15 @@ useEffect(() => {
             {/* Stories  */}
             <div className="stories-card border rounded-lg col-span-2 flex pl-4">
               <div className="mr-4 ">
-                <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
+                <img src="https://robohash.org/consequunturautconsequatur.png" alt="wetroverse" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
                 <p className="text-center mt-2">Divine</p>
               </div>
               <div className="mr-4 ">
-                <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
+                <img src="https://robohash.org/consequunturautconsequatur.png" alt="wetroverse" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
                 <p className="text-center mt-2">Divine</p>
               </div>
               <div className="mr-4 ">
-                <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
+                <img src="https://robohash.org/consequunturautconsequatur.png" alt="wetroverse" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
                 <p className="text-center mt-2">Divine</p>
               </div>
               <div className="mr-4 ">
