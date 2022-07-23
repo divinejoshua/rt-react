46bda5b45246307f28343df2ea594d37ad818e23
divinejoshua <divineuche399@gmail.com>
2022-07-23 01:55:03 +0100

    Made changes

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import MainHeader from "../components/MainHeader";
 
 export default function PostView() {
@@ -23,7 +24,7 @@ const getUsers = () => {
   
 //USE EFFECT
 useEffect(() => {
-  console.log("post")
+  console.log("home")
 
   //Set loding page to false
   setTimeout(() => {  setisLoadingPage(false) }, 3000);
@@ -53,6 +54,43 @@ useEffect(() => {
         <div className="grid grid-cols-3 gap-4 mt-10">
 
 
+            {/* Stories  */}
+            <div className="stories-card border rounded-lg col-span-2 flex pl-4">
+              <div className="mr-4 ">
+                <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
+                <p className="text-center mt-2">Divine</p>
+              </div>
+              <div className="mr-4 ">
+                <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
+                <p className="text-center mt-2">Divine</p>
+              </div>
+              <div className="mr-4 ">
+                <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
+                <p className="text-center mt-2">Divine</p>
+              </div>
+              <div className="mr-4 ">
+                <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
+                <p className="text-center mt-2">Divine</p>
+              </div>
+              <div className="mr-4 ">
+                <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
+                <p className="text-center mt-2">Divine</p>
+              </div>
+              <div className="mr-4 ">
+                <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
+                <p className="text-center mt-2">Divine</p>
+              </div>
+              <div className="mr-4 ">
+                <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
+                <p className="text-center mt-2">Divine</p>
+              </div>
+              <div className="mr-4 ">
+                <img src="https://robohash.org/consequunturautconsequatur.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full story-image"></img>
+                <p className="text-center mt-2">Divine</p>
+              </div>
+              
+                  
+            </div>
 
             {/* Profile card */}
             <div className="ml-3">
@@ -73,6 +111,7 @@ useEffect(() => {
           {/* First column  */}
           <div className="mt-4">
 
+            
             {/* Post */}
             <div className="post-card border rounded-lg pt-3 mb-4">
 
@@ -80,7 +119,7 @@ useEffect(() => {
               <div className="mb-5">
                 <div className="float-left"><img src="https://robohash.org/namquaerataut.png" className="ml-3 border-2 p-1 rounded-full user-post-img"></img></div>
                 <div className="float-left mt-3 font-bold ml-2">divine.er</div>
-                <div className="float-right mt-3 text-sky-400 mr-5 cursor-pointer">View</div>
+                <Link to="/post/1"><div className="float-right mt-3 text-sky-400 mr-5 cursor-pointer">View</div></Link>
               </div>
 
               {/* Post image  */}
@@ -100,8 +139,8 @@ useEffect(() => {
               </div>
             </div>
 
+            
 
-         
           </div>
 
 
@@ -109,92 +148,46 @@ useEffect(() => {
           <div className="mt-4">
 
             
-            {/* Post */}
-            <div className="post-card border rounded-lg pt-3 mb-4">
+           <h4 className="ml-4 mt-1 font-color-777">Comments</h4>
 
-              {/* Post user  */}
-              <div className="mb-5">
-                <div className="float-left"><img src="https://robohash.org/namquaerataut.png" className="ml-3 border-2 p-1 rounded-full user-post-img"></img></div>
-                <div className="float-left mt-3 font-bold ml-2">divine.er</div>
-                <div className="float-right mt-3 text-sky-400 mr-5 cursor-pointer">View</div>
-              </div>
+                {/* Comments  */}
+                <div className="flex">
+                    <img src="https://robohash.org/quisequienim.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full comment-card-image"></img>
+                    <div>
+                    <h4 className="font-bold mt-7 ml-2">divine.er</h4>
+                    <h4 className="ml-2 font-color-777 font-size-x-small">You amaze me!</h4>
+                    </div>
+                </div>
 
-              {/* Post image  */}
-              <div>
-                <br></br>
-                <img src="https://picsum.photos/300/300?" className="post-img mt-4" alt="wetroverse"></img>
-              </div>
+                <div className="flex">
+                    <img src="https://robohash.org/quisequienim.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full comment-card-image"></img>
+                    <div>
+                    <h4 className="font-bold mt-7 ml-2">divine.er</h4>
+                    <h4 className="ml-2 font-color-777 font-size-x-small">You amaze me!</h4>
+                    </div>
+                </div>
 
-              {/* Likes and caption */}
-              <div className="p-6">
-                  <div className="mb-3 font-color-777 mr-5 cursor-pointer">Like &nbsp;&nbsp;•&nbsp;&nbsp;200 </div>
-                  His mother had always taught him 
-                <br></br>
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span>
-              </div>
-            </div>
+                <div className="flex">
+                    <img src="https://robohash.org/quisequienim.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full comment-card-image"></img>
+                    <div>
+                    <h4 className="font-bold mt-7 ml-2">divine.er</h4>
+                    <h4 className="ml-2 font-color-777 font-size-x-small">You amaze me!</h4>
+                    </div>
+                </div>
 
+                <div className="flex">
+                    <img src="https://robohash.org/quisequienim.png" className="cursor-pointer mt-6 border-2 p-1 rounded-full comment-card-image"></img>
+                    <div>
+                    <h4 className="font-bold mt-7 ml-2">divine.er</h4>
+                    <h4 className="ml-2 font-color-777 font-size-x-small">Divine Erhomonsele</h4>
+                    </div>
+                    <p className='ml-36 font-color-c4 float-right mt-8 mr-6 cursor-pointer'>Delete</p>
 
-              {/* Post */}
-              <div className="post-card border rounded-lg pt-3 mb-4">
-
-              {/* Post user  */}
-              <div className="mb-5">
-                <div className="float-left"><img src="https://robohash.org/namquaerataut.png" className="ml-3 border-2 p-1 rounded-full user-post-img"></img></div>
-                <div className="float-left mt-3 font-bold ml-2">divine.er</div>
-                <div className="float-right mt-3 text-sky-400 mr-5 cursor-pointer">View</div>
-              </div>
-
-              {/* Post image  */}
-              <div>
-                <br></br>
-                <img src="https://picsum.photos/300/300?" className="post-img mt-4" alt="wetroverse"></img>
-              </div>
-
-              {/* Likes and caption */}
-              <div className="p-6">
-                  <div className="mb-3 font-color-777 mr-5 cursor-pointer">Like &nbsp;&nbsp;•&nbsp;&nbsp;200 </div>
-                  His mother had always taught him 
-                <br></br>
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span>
-              </div>
-              </div>
-
-            
-            
-            {/* Post */}
-            <div className="post-card border rounded-lg pt-3 mb-4">
-
-              {/* Post user  */}
-              <div className="mb-5">
-                <div className="float-left"><img src="https://robohash.org/namquaerataut.png" className="ml-3 border-2 p-1 rounded-full user-post-img"></img></div>
-                <div className="float-left mt-3 font-bold ml-2">divine.er</div>
-                <div className="float-right mt-3 text-sky-400 mr-5 cursor-pointer">View</div>
-              </div>
-
-              {/* Post image  */}
-              <div>
-                <br></br>
-                <img src="https://picsum.photos/300/300?" className="post-img mt-4" alt="wetroverse"></img>
-              </div>
-
-              {/* Likes and caption */}
-              <div className="p-6">
-                  <div className="mb-3 font-color-777 mr-5 cursor-pointer">Like &nbsp;&nbsp;•&nbsp;&nbsp;200 </div>
-                  His mother had always taught him 
-                <br></br>
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span>
-              </div>
-            </div>
+                </div>
 
 
-            
+
+
           </div>
 
 
