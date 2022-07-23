ef761536fd619a475b7a26c2ff02ded58a3be0a7
divinejoshua <divineuche399@gmail.com>
2022-07-23 17:59:49 +0100

    added post feed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -77,10 +77,16 @@ useEffect(() => {
           {/* Posts  */}
 
           {/* First column  */}
-          <div className="mt-4">
+          <div className="mt-4 col-span-2">
+
+
+
+          <div className="grid grid-cols-2 gap-4 mt-10">
+
+
 
             {/* Post */}
-            <div className="post-card border rounded-lg pt-3 mb-4">
+            <div className="post-card border rounded-lg pt-3 mb-4 col-span-1">
 
               {/* Post user  */}
               <div className="mb-5">
@@ -107,158 +113,12 @@ useEffect(() => {
             </div>
 
 
-              {/* Post */}
-              <div className="post-card border rounded-lg pt-3 mb-4">
-
-              {/* Post user  */}
-              <div className="mb-5">
-                <div className="float-left"><img src="https://robohash.org/namquaerataut.png" className="ml-3 border-2 p-1 rounded-full user-post-img"></img></div>
-                <div className="float-left mt-3 font-bold ml-2">divine.er</div>
-                <Link to="/post/1"><div className="float-right mt-3 text-sky-400 mr-5 cursor-pointer">View</div></Link>
-              </div>
-
-              {/* Post image  */}
-              <div>
-                <br></br>
-                <img src="https://picsum.photos/300/300?" className="post-img mt-4" alt="wetroverse"></img>
-              </div>
-
-              {/* Likes and caption */}
-              <div className="p-6">
-                  <div className="mb-3 font-color-777 mr-5 cursor-pointer">Like &nbsp;&nbsp;•&nbsp;&nbsp;200 </div>
-                  His mother had always taught him 
-                <br></br>
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span>
-              </div>
-              </div>
-
-            
-            
-            {/* Post */}
-            <div className="post-card border rounded-lg pt-3 mb-4">
-
-              {/* Post user  */}
-              <div className="mb-5">
-                <div className="float-left"><img src="https://robohash.org/namquaerataut.png" className="ml-3 border-2 p-1 rounded-full user-post-img"></img></div>
-                <div className="float-left mt-3 font-bold ml-2">divine.er</div>
-                <Link to="/post/1"><div className="float-right mt-3 text-sky-400 mr-5 cursor-pointer">View</div></Link>
-              </div>
-
-              {/* Post image  */}
-              <div>
-                <br></br>
-                <img src="https://picsum.photos/300/300?" className="post-img mt-4" alt="wetroverse"></img>
-              </div>
-
-              {/* Likes and caption */}
-              <div className="p-6">
-                  <div className="mb-3 font-color-777 mr-5 cursor-pointer">Like &nbsp;&nbsp;•&nbsp;&nbsp;200 </div>
-                  His mother had always taught him 
-                <br></br>
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span>
-              </div>
-            </div>
-
-            
 
           </div>
 
-
-          {/* second column  */}
-          <div className="mt-4">
-
-            
-            {/* Post */}
-            <div className="post-card border rounded-lg pt-3 mb-4">
-
-              {/* Post user  */}
-              <div className="mb-5">
-                <div className="float-left"><img src="https://robohash.org/namquaerataut.png" className="ml-3 border-2 p-1 rounded-full user-post-img"></img></div>
-                <div className="float-left mt-3 font-bold ml-2">divine.er</div>
-                <Link to="/post/1"><div className="float-right mt-3 text-sky-400 mr-5 cursor-pointer">View</div></Link>
-              </div>
-
-              {/* Post image  */}
-              <div>
-                <br></br>
-                <img src="https://picsum.photos/300/300?" className="post-img mt-4" alt="wetroverse"></img>
-              </div>
-
-              {/* Likes and caption */}
-              <div className="p-6">
-                  <div className="mb-3 font-color-777 mr-5 cursor-pointer">Like &nbsp;&nbsp;•&nbsp;&nbsp;200 </div>
-                  His mother had always taught him 
-                <br></br>
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span>
-              </div>
-            </div>
+      </div>
 
 
-              {/* Post */}
-              <div className="post-card border rounded-lg pt-3 mb-4">
-
-              {/* Post user  */}
-              <div className="mb-5">
-                <div className="float-left"><img src="https://robohash.org/namquaerataut.png" className="ml-3 border-2 p-1 rounded-full user-post-img"></img></div>
-                <div className="float-left mt-3 font-bold ml-2">divine.er</div>
-                <Link to="/post/1"><div className="float-right mt-3 text-sky-400 mr-5 cursor-pointer">View</div></Link>
-              </div>
-
-              {/* Post image  */}
-              <div>
-                <br></br>
-                <img src="https://picsum.photos/300/300?" className="post-img mt-4" alt="wetroverse"></img>
-              </div>
-
-              {/* Likes and caption */}
-              <div className="p-6">
-                  <div className="mb-3 font-color-777 mr-5 cursor-pointer">Like &nbsp;&nbsp;•&nbsp;&nbsp;200 </div>
-                  His mother had always taught him 
-                <br></br>
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span>
-              </div>
-              </div>
-
-            
-            
-            {/* Post */}
-            <div className="post-card border rounded-lg pt-3 mb-4">
-
-              {/* Post user  */}
-              <div className="mb-5">
-                <div className="float-left"><img src="https://robohash.org/namquaerataut.png" className="ml-3 border-2 p-1 rounded-full user-post-img"></img></div>
-                <div className="float-left mt-3 font-bold ml-2">divine.er</div>
-                <Link to="/post/1"><div className="float-right mt-3 text-sky-400 mr-5 cursor-pointer">View</div></Link>
-              </div>
-
-              {/* Post image  */}
-              <div>
-                <br></br>
-                <img src="https://picsum.photos/300/300?" className="post-img mt-4" alt="wetroverse"></img>
-              </div>
-
-              {/* Likes and caption */}
-              <div className="p-6">
-                  <div className="mb-3 font-color-777 mr-5 cursor-pointer">Like &nbsp;&nbsp;•&nbsp;&nbsp;200 </div>
-                  His mother had always taught him 
-                <br></br>
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
-                <span className="text-default">#endregion&nbsp;&nbsp;</span>
-              </div>
-            </div>
-
-
-            
-          </div>
 
 
 
