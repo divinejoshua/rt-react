d877b56a50bfdb7a8a30dd182cf1e3ef24e756f9
divinejoshua <divineuche399@gmail.com>
2022-07-23 16:59:33 +0100

    made changes

--- src/components/MainSidebar.js
+++ src/components/MainSidebar.js
@@ -23,19 +23,23 @@ export default function MainSidebar() {
 
   return (
     <div>
-        <div className="mt-4 ml-3">
+        <div className="mt-4 ml-3 main-sidebar">
             <p className="font-color-777 mb-3">Suggestions for you</p>
 
           {/* Map through the users list  */}
           
             {Array.isArray(users.users) ? users.users.map(user => (
-              <div className="flex" key={user.id}>
-                    <img src={user.image} className="cursor-pointer mt-6 border-2 p-1 rounded-full users-card-image"></img>
-                    <div>
+              <div className="" key={user.id}>
+                    <img src={user.image} className="float-left cursor-pointer mt-6 border-2 p-1 rounded-full users-card-image"></img>
+                    <div className='float-left'>
                       <h4 className="font-bold mt-6 ml-2">{user.username}</h4>
                       <h4 className="ml-2 font-color-777 font-size-x-small">{user.firstName} {user.lastName}</h4>
                     </div>
                   <p className='ml-36 text-sky-400 font-bold float-right mt-8 mr-6 cursor-pointer'>Follow</p>
+                  <br></br>
+                  <br></br>
+                  <br></br>
+              <br></br>
 
               </div>
              )) : null }
