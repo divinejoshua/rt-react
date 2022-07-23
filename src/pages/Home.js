ff850eb7dcd70023a77b07003000a1f6bc49e279
divinejoshua <divineuche399@gmail.com>
2022-07-23 01:26:59 +0100

    Made changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -111,7 +111,7 @@ useEffect(() => {
           <div className="mt-4">
 
             {/* Post */}
-            <div className="post-card border rounded-lg pt-3">
+            <div className="post-card border rounded-lg pt-3 mb-4">
 
               {/* Post user  */}
               <div className="mb-5">
@@ -137,6 +137,64 @@ useEffect(() => {
               </div>
             </div>
 
+
+              {/* Post */}
+              <div className="post-card border rounded-lg pt-3 mb-4">
+
+              {/* Post user  */}
+              <div className="mb-5">
+                <div className="float-left"><img src="https://robohash.org/namquaerataut.png" className="ml-3 border-2 p-1 rounded-full user-post-img"></img></div>
+                <div className="float-left mt-3 font-bold ml-2">divine.er</div>
+                <div className="float-right mt-3 text-sky-400 mr-5 cursor-pointer">View</div>
+              </div>
+
+              {/* Post image  */}
+              <div>
+                <br></br>
+                <img src="https://picsum.photos/300/300?" className="post-img mt-4" alt="wetroverse"></img>
+              </div>
+
+              {/* Likes and caption */}
+              <div className="p-6">
+                  <div className="mb-3 font-color-777 mr-5 cursor-pointer">Like &nbsp;&nbsp;•&nbsp;&nbsp;200 </div>
+                  His mother had always taught him 
+                <br></br>
+                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
+                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
+                <span className="text-default">#endregion&nbsp;&nbsp;</span>
+              </div>
+              </div>
+
+            
+            
+            {/* Post */}
+            <div className="post-card border rounded-lg pt-3 mb-4">
+
+              {/* Post user  */}
+              <div className="mb-5">
+                <div className="float-left"><img src="https://robohash.org/namquaerataut.png" className="ml-3 border-2 p-1 rounded-full user-post-img"></img></div>
+                <div className="float-left mt-3 font-bold ml-2">divine.er</div>
+                <div className="float-right mt-3 text-sky-400 mr-5 cursor-pointer">View</div>
+              </div>
+
+              {/* Post image  */}
+              <div>
+                <br></br>
+                <img src="https://picsum.photos/300/300?" className="post-img mt-4" alt="wetroverse"></img>
+              </div>
+
+              {/* Likes and caption */}
+              <div className="p-6">
+                  <div className="mb-3 font-color-777 mr-5 cursor-pointer">Like &nbsp;&nbsp;•&nbsp;&nbsp;200 </div>
+                  His mother had always taught him 
+                <br></br>
+                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
+                <span className="text-default">#endregion&nbsp;&nbsp;</span> 
+                <span className="text-default">#endregion&nbsp;&nbsp;</span>
+              </div>
+            </div>
+
+            
 
           </div>
 
