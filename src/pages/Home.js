c60988ca173dd362bfa8a2195ad65c51141149a6
divinejoshua <divineuche399@gmail.com>
2022-08-02 00:48:28 +0100

    More changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,19 +18,25 @@ const observer = useRef()
 // Get the last element to be rendered in the list 
 const lastElementRef = useCallback(node=>{
 
-  // Return if a request is loading 
-  if(isPending) return
+    // Return if a request is loading 
+    if(isPending) return
 
-  // If there is a new last element, disconnect from previous last element 
-  if(observer.current) observer.current.disconnect()
+    // If there is a new last element, disconnect from previous last element 
+    if(observer.current) observer.current.disconnect()
 
-  // Observe the new element 
-  observer.current = new IntersectionObserver(entries=>{
+    // Observe the new element 
+    observer.current = new IntersectionObserver(entries=>{
 
-  })
-  if (node) observer.current.observe(node)
-  console.log(node)
-})
+      // Check if last element is visible 
+      if(entries[0].isIntersecting){
+        console.log('Visible')
+      }
+
+      
+    })
+    if (node) observer.current.observe(node)
+    console.log(node)
+},[isPending])
 
 
 //METHODS
