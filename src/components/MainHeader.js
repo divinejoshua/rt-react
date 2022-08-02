c80fa3b8b7c6209a02681070563f1109bfc645cc
divinejoshua <divineuche399@gmail.com>
2022-08-02 19:23:12 +0100

    Made changes

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -7,9 +7,12 @@ export default function MainHeader() {
 
   //STATES
   const [isLoading, setisLoading] = useState(false);
+  const [clickSearchFirst, setclickSearchFirst] = useState(true);
 
   // React hook form 
-const { register, handleSubmit, formState: { errors, isDirty }  } = useForm({mode: 'all'});
+  const { register, handleSubmit, watch, formState: { errors, isDirty }  } = useForm({mode: 'all'});
+const watchFields = watch(["search"]);
+
 
 
 //USE EFFECT
@@ -34,8 +37,8 @@ useEffect(() => {
         {/* Search box  */}
         <div className=' basis-1/3 pt-4'>
           <center>
-             <form onSubmit={handleSubmit()} noValidate>
-              <input className='search-box pl-5 rounded-lg focus:outline-none focus:border-default focus:ring-default focus:ring-0.5 focus:border-100 transition duration-0 hover:duration-150' 
+             <form onSubmit={handleSubmit()} noValidate >
+              <input  className='search-box pl-5 rounded-lg focus:outline-none focus:border-default focus:ring-default focus:ring-0.5 focus:border-100 transition duration-0 hover:duration-150' 
                       type='text' placeholder='Search'
                       {...register('search', { required: true})}
                       >
@@ -55,12 +58,14 @@ useEffect(() => {
           </div>
       </div>
 
+      {watchFields[0] ? 
       <div className='header-content mt-4'>
       <div className='suggestion-box'>
-        {errors.search ? 'true' : 'false'}
+        {watchFields[0] ? 'true' : "false"}
         </div>
       </div>
-
+      : ''
+      }
 
 
     </nav>
