25fab920e0a71e953df3559bb1f5ae4e13bd07d2
divinejoshua <divineuche399@gmail.com>
2022-08-05 00:25:11 +0100

    Added more

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './user'
+import storiesReducer from './stories'
 
 export const store = configureStore({
   reducer: {
     userDetails : userReducer,
+    userStories : storiesReducer,
   },
 })
