c7e841c38b8cf0b9b90b3bfefedb6ddb2afdbbf6
divinejoshua <divineuche399@gmail.com>
2022-08-05 01:25:04 +0100

    Made changes

--- src/redux/stories.js
+++ src/redux/stories.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 // STATES 
 const initialState = {
-  stories: "yoo",
+  stories: [],
 }
 
 export const userStories = createSlice({
@@ -10,15 +10,16 @@ export const userStories = createSlice({
   initialState,
   reducers: {
     
-    // Register user email from login / register 
-    registerEmail: (state, action) => {
+    //User stories list 
+    registerStories: (state, action) => {
       state.stories = action.payload
+      console.log(state.stories)
     },
 
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { registerEmail } = userStories.actions
+export const { registerStories } = userStories.actions
 
 export default userStories.reducer
