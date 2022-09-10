c925165e87746383d1989ece46f59ac3e87c3726
divinejoshua <divineuche399@gmail.com>
2022-09-10 16:39:10 +0100

    Made changes

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ import HomeView from './pages/Home';
 import PostView from './pages/Post';
 import PageNotFound from './pages/Error/404';
 import AuthToken from './components/AuthToken';
+import { AuthProvider} from './context/AuthContext';
 import UseEffectHook from "./utils/UseEffectHook";
-import axios from "./api/axios";
-
 
 function App() {
 
@@ -24,31 +23,31 @@ function App() {
 
       <div className="App">
 
+        {/* Auth provider  */}
+          <AuthProvider>
+                <Routes>
 
-          <Routes>
+                    <Route element={<AuthToken/>}>
+                          {/* Home  */}
+                          <Route path="/" element={<HomeView/>}/>
+                          {/* Post view  */}
+                          <Route path="/post/:id" element={<PostView/>}/>
+                    </Route>
 
-          
-      <Route element={<AuthToken/>}>
-            {/* Home  */}
-            <Route path="/" element={<HomeView/>}/>
-            {/* Post view  */}
-            <Route path="/post/:id" element={<PostView/>}/>
-      </Route>
-
-            {/* Login  */}
-            <Route path="/accounts/login" element={<LoginView/>}/>
-
-
-            {/* Register  */}
-            <Route path="/accounts/register" element={<RegisterView/>}/>
-
-            {/* <Route path="accounts/login/:id" element={<LoginView/>}/> Optional Paramater */}
-
-            {/* 404  */}
-            <Route path="*" element={<PageNotFound/>}/>
+                      {/* Login  */}
+                      <Route path="/accounts/login" element={<LoginView/>}/>
 
 
-          </Routes>
+                      {/* Register  */}
+                      <Route path="/accounts/register" element={<RegisterView/>}/>
+
+                      {/* <Route path="accounts/login/:id" element={<LoginView/>}/> Optional Paramater */}
+
+                      {/* 404  */}
+                      <Route path="*" element={<PageNotFound/>}/>
+
+                </Routes>
+          </AuthProvider>
    
       </div>
       
